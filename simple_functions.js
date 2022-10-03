16c4975d1382595ef519602481fd96fed6705d08
kevinelong <kevinelong@gmail.com>
2022-10-03 08:50:58 -0700

    log the things

---
+++ simple_functions.js
@@ -0,0 +1,20 @@
+//DEFINE THREE NAMED FUNCTION 
+function favoriteThings(){
+    console.log("NAME: Kevin Ernest Long");
+    console.log("MOVIE: Being There");
+    console.log("MUSICIAN: Frank Zappa");
+    console.log("BAND: Death for Cutie");
+}
+
+function whyImHere(){
+    console.log("WHY: Contribute to the next generation.")
+}
+
+function favoritePlace(){
+    console.log("PLACE: Oregon Beaches and Mountains")
+}
+
+//CALL ALL THREE FUNCTIONS BY NAME
+favoriteThings();
+whyImHere();
+favoritePlace();
