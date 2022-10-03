2f42f87466a72985f44c174c6dbac224443c949f
kevinelong <kevinelong@gmail.com>
2022-10-03 12:26:19 -0700

    parsing

---
+++ Strings/parsing.js
@@ -0,0 +1,28 @@
+
+// SECTION 2-12 EXERCISE 1
+
+// parse - VERB: cut into pieces - like paring-knife, parcel, parsec
+
+function parseAndDisplayName(name){
+    const SPACE = " ";
+    let whereToCut = name.indexOf(SPACE); // WHERE IS THE SPACE
+    let leftSide = name.substring(0, whereToCut); //TWO PARAMETERS - START AND STOP
+    let rightSide = name.substring(whereToCut); // ONE PARAMETER - WHERE TO START, (IMPLIED TO THE END)
+
+    console.log("NAME: " + name);
+    console.log("FIRST: " + leftSide);
+    console.log("LAST: " + rightSide);
+}
+
+/*EXPECTED
+Name: Brenda Kaye
+First name: Brenda
+Last name: Kaye 
+*/
+
+let name = "Brenda Kaye"; 
+parseAndDisplayName(name)
+
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
