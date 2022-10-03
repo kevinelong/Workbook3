9ebca1fc7675701beaf557953a161cd906c6c9e2
kevinelong <kevinelong@gmail.com>
2022-10-03 10:32:08 -0700

    added section and exercise and page number

--- more_functions.js
+++ more_functions.js
@@ -5,8 +5,9 @@ console.log(123)
 console.log(-123)
 
 console.log(Math.abs(123))
-console.log(Math.abs(-123))
+console.log(Math.abs(-123))// always positive - never negative, (inverts makes opposite if negative)
 
+// 1-12 Exercise #3 from page 16
 function displayReceipt(totalDue, amountPaid){
     console.log("TOTAL DUE: $" + totalDue);
     console.log("AMOUNT PAID: $" + amountPaid);
